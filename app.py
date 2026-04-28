ecfa51cce174753da024c04ec0b8718845e1b253
ItamarHassin <itamar.hassin@gmail.com>
2026-04-28 09:02:09 +0300

    Add comments for clarity in app.py

--- app.py
+++ app.py
@@ -19,34 +19,34 @@ class ResizePad:
         self.size = size
 
     def __call__(self, img):
-        w, h = img.size
-        scale = self.size / max(w, h)
-        new_w, new_h = int(w * scale), int(h * scale)
-        img = F.resize(img, (new_h, new_w))
+        w, h = img.size # current width and height of the photo
+        scale = self.size / max(w, h) #new desired scale of the photo
+        new_w, new_h = int(w * scale), int(h * scale) #rescaling the width and the height
+        img = F.resize(img, (new_h, new_w)) #resizing the photo according to the new scale
 
-        pad_w = self.size - new_w
-        pad_h = self.size - new_h
-        left = pad_w // 2
-        right = pad_w - left
-        top = pad_h // 2
-        bottom = pad_h - top
-        return F.pad(img, [left, top, right, bottom])
+        pad_w = self.size - new_w # adding the needed padding
+        pad_h = self.size - new_h # adding the needed padding
+        left = pad_w // 2# adding the needed padding for each side
+        right = pad_w - left # adding the needed padding for each side
+        top = pad_h // 2 # adding the needed padding for each side
+        bottom = pad_h - top# adding the needed padding for each side
+        return F.pad(img, [left, top, right, bottom])# padding
 
 
 #Static vatiables
-BASE_DIR = Path(__file__).resolve().parent
-VIDS_DIR = BASE_DIR / "Vids to test"
+BASE_DIR = Path(__file__).resolve().parent#
+VIDS_DIR = BASE_DIR / "Vids to test"#Directory for vids
 
-CUSTOM_MP = BASE_DIR / "DistractModel3.0.pth"
-TRANSFER_MP = BASE_DIR / "DistractModelTransfer2.pth"
+CUSTOM_MP = BASE_DIR / "DistractModel3.0.pth" #Distraction model directory
+TRANSFER_MP = BASE_DIR / "DistractModelTransfer2.pth"#Distraction model tranfer directory
 
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu" #Where to run the code?
 
 VAL_TF_CUSTOM = transforms.Compose([
-    ResizePad(256),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]),
+    ResizePad(256), #resizing
+    transforms.ToTensor(), #Trasforming to tensor
+    transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]), #normalizing according to known values
 ])
 
 
@@ -68,19 +68,22 @@ class CNN_Distract(nn.Module):
             nn.Conv2d(3,32,3,padding=1,bias=False), nn.BatchNorm2d(32), nn.ReLU(),
             nn.Conv2d(32,32,3,padding=1,bias=False), nn.BatchNorm2d(32), nn.ReLU(),
             nn.MaxPool2d(2),
+            #first convection layer, from 3 channels to 32, then pooling the data to shrink dimentions (128)
 
             nn.Conv2d(32,64,3,padding=1,bias=False), nn.BatchNorm2d(64), nn.ReLU(),
             nn.Conv2d(64,64,3,padding=1,bias=False), nn.BatchNorm2d(64), nn.ReLU(),
             nn.MaxPool2d(2),
+            #Second convection layer, from 32 channels to 64, then pooling the data to shrink dimentions (64)
 
             nn.Conv2d(64,128,3,padding=1,bias=False), nn.BatchNorm2d(128), nn.ReLU(),
             nn.Conv2d(128,128,3,padding=1,bias=False), nn.BatchNorm2d(128), nn.ReLU(),
             nn.MaxPool2d(2),
+            #Third convection layer, from 64 channels to 128, then pooling the data to shrink dimentions (32)
 
-            nn.AdaptiveAvgPool2d(1),
-            nn.Flatten(),
-            nn.Dropout(0.2),
-            nn.Linear(128, num_classes)
+            nn.AdaptiveAvgPool2d(1), #pooling to 128x1x1
+            nn.Flatten(),#flattening to 128 
+            nn.Dropout(0.2), #removing neurons
+            nn.Linear(128, num_classes) #last linear
         )
 
     def forward(self, x):
@@ -128,9 +131,9 @@ def person_present(img_rgb, model, score_thr=0.6):
 def classify(img_rgb, model, transforms):
     with torch.no_grad():
         if not isinstance(img_rgb, Image.Image):
-            img_rgb = Image.fromarray(img_rgb)
-        out = model(transforms(img_rgb).unsqueeze(0).to(DEVICE))
-        return int(out.argmax(dim=1).item())
+            img_rgb = Image.fromarray(img_rgb)  #makes sure its a PIL object
+        out = model(transforms(img_rgb).unsqueeze(0).to(DEVICE))#running frame through model
+        return int(out.argmax(dim=1).item())#getting prediction
 
 
 def getlabel(pred):
@@ -163,16 +166,16 @@ def main():
     with st.sidebar:
         st.header("Controls")
 
-        video_files =[p for p in VIDS_DIR.iterdir()]
+        video_files =[p for p in VIDS_DIR.iterdir()] #adding video files
 
         input_source = st.selectbox(
             "Input Source",
-            ["Webcam"] + [v.name for v in video_files]
+            ["Webcam"] + [v.name for v in video_files] # adding video sources
         )
 
         model_choice = st.radio(
             "Model",
-            ["Custom CNN", "Transfer (ResNet18)"],
+            ["Custom CNN", "Transfer (ResNet18)"], #adding model choices
             horizontal=True
         )
 
@@ -199,13 +202,13 @@ def main():
 
     #loading the models
     person_model, custom_model, transfer_model = load_models()
-    if model_choice == "Custom CNN":
+    if model_choice == "Custom CNN": #using correct model
         model = custom_model
     else:
         model = transfer_model
 
 
-    if model_choice == "Custom CNN":
+    if model_choice == "Custom CNN":#using correct transforms
         tf = VAL_TF_CUSTOM
     else:
         tf = VAL_TF_TRANSFER
@@ -215,9 +218,9 @@ def main():
 
     # Opening the video source
     if input_source == "Webcam":
-        capture = cv2.VideoCapture(0)
+        capture = cv2.VideoCapture(0) #connecting to webcam
     else:
-        capture = cv2.VideoCapture(str(VIDS_DIR / input_source))
+        capture = cv2.VideoCapture(str(VIDS_DIR / input_source)) #connection to relevant chosen video
 
 
 
@@ -229,18 +232,18 @@ def main():
         if not ret:
             capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
             continue
-        frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+        frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB) #correcting to the rgb trained on
 
 
         #getting the prediction
         if person_present(frame_rgb, person_model):
 
-            prediction = classify(frame_rgb, model, tf)
+            prediction = classify(frame_rgb, model, tf) #gettig prediction
 
             if (prediction != 4 and last_prediction != 4):
-                playsound(os.path.join(BASE_DIR, "Sounds","beep.mp3"))
+                playsound(os.path.join(BASE_DIR, "Sounds","beep.mp3")) #playing sound
             
-            label = f"{getlabel(prediction)}"
+            label = f"{getlabel(prediction)}" #showing label
             last_prediction = prediction
         else:
             label = "NO DRIVER"
@@ -248,7 +251,7 @@ def main():
 
 
         #displaying
-        cv2.putText(frame_bgr, label, (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        cv2.putText(frame_bgr, label, (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)#displaying in green
         video_box.image(
             cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB),
             width='stretch'
